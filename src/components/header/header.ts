eae7ed1f6cbdd28d5c588325dde25874c90a49af
Renee Vrantsidis <renee.vrantsidis@rangle.io>
2017-02-03 16:05:56 -0500

    Light styling changes, just while I'm at it...

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -4,13 +4,13 @@ import { Component } from '@angular/core';
   selector: 'rio-header',
   styles: [require('./header.css')],
   template: `
-    <header class="bg-teal p1 mb3">
+    <header class="bg-teal p2 mb3 app-header">
       <div class="app-container flex items-center justify-center">
-        <img src="../../assets/wizard-icon.png"
+        <img src="../../assets/wizard-icon.svg"
           alt="Wizards Wizard"
           class="inline-block logo"
           height="1em">
-        <h1 class="inline-block my0">Wizards Wizard</h1>
+        <h1 class="inline-block my0 title">Wizards Wizard</h1>
       </div>
     </header>
   `
