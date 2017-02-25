77d82a01da0471bdf0f7bfb19d18b3b988b7fb06
Daniel Figueiredo <danielfigueiredoc@gmail.com>
2017-02-25 12:30:50 -0500

    Small cleanups on validations (#11)
    Extract createFormFieldSelector usages to a variable, otherwise the function will always be generated thereafter never memoized
    Added generic type to createFormFieldSelector so typescript knows which type expect as the return of the function
    Created isBetweenNumber validator so that ageValidation can be better composed

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,29 +1,42 @@
-import { isEmpty, pipe, filter, isNil } from 'ramda';
+import {
+  isEmpty,
+  pipe,
+  filter,
+  isNil,
+  gt,
+  lt,
+  gte,
+  lte,
+  find
+} from 'ramda';
 
-export const isValid = (...validators): any => {
-  return (arg: any) => {
-    // TODO: Find appropriate function for this...
-    for (const val of validators) {
-      if (!val(arg)) {
-        return false;
-      }
-    }
-    return true;
-  };
-};
+export const isValid = (...validators): any =>
+  (arg: any) => isNil(find(val => !val(arg), validators));
 
 // Validator functions
 export const maxNumberValidation = (max: number) =>
-  (value: number) => value < max;
+  (value: number) => lt(value, max);
+
+export const minNumberValidation = (min: number) =>
+  (value: number) => gt(value, min);
+
 export const maxStringLengthValidation = (max: number) =>
-  (value: string) => value.length < max;
+  (value: string) => value
+    ? maxNumberValidation(max)(value.length)
+    : true;
+
 export const minStringLengthValidation = (min: number) =>
-  (value: string) => value.length > min;
-export const arrayNotEmptyValidation = () =>
-  (value: string[]) => {
-    const hasValue = pipe(
-      filter(isNil),
-      isEmpty
-    );
-    return isEmpty(value) ? false : hasValue(value);
-  };
+  (value: string) => value
+    ? minNumberValidation(min)(value.length)
+    : true;
+
+export const arrayNotEmptyValidation = (value: string[]) => {
+  const hasValue = pipe(
+    filter(isNil),
+    isEmpty
+  );
+  return isEmpty(value) ? false : hasValue(value);
+};
+
+export const isBetweenNumber = (min, max) =>
+  (value: number) => gte(value, min) && lte(value, max);
